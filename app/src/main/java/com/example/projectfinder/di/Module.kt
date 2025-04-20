2affecc9ea2b7d730086bf47a0375bd1efe606f4
Ebite Stephen <ebitestephen773@gmail.com>
2025-04-20 13:26:14 -0700

    set up hilt injections

--- app/src/main/java/com/example/projectfinder/di/Module.kt
+++ app/src/main/java/com/example/projectfinder/di/Module.kt
@@ -1,4 +1,36 @@
 package com.example.projectfinder.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.projectfinder.projectfinder.data.local.ProjectDatabase
+import com.example.projectfinder.projectfinder.data.repository.ProjectRepositoryImpl
+import com.example.projectfinder.projectfinder.domain.repository.ProjectRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object Module {
+
+
+    @Provides
+    @Singleton
+    fun provideProjectDatabase(app: Application): ProjectDatabase {
+        return Room.databaseBuilder(
+            app,
+            ProjectDatabase::class.java,
+            "project.db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProjectRepository( db: ProjectDatabase ): ProjectRepository {
+        return ProjectRepositoryImpl(db.projectDao)
+    }
 }
